03a41c9514453a5fe67d41d28b4c15b24fbf7502
chlbri <chlbri.blac@gmail.com>
2021-02-07 22:14:38 +0000

    Make Validator base class abstract

--- dist/Interfaces/Validators/Validator.d.ts
+++ dist/Interfaces/Validators/Validator.d.ts
@@ -1,6 +1,6 @@
 import { Exception } from "../exceptions";
 declare type Condition<T = any> = (arg: T) => boolean;
-declare class Validator<T> {
+declare abstract class Validator<T> {
     validate: Condition<T>;
     exception: Exception;
     constructor(validate: Condition<T>, exception?: Exception);
--- src/Interfaces/validators/Validator.ts
+++ src/Interfaces/validators/Validator.ts
@@ -2,7 +2,7 @@ import { DEFAULT_EXCEPTION, Exception } from "../exceptions";
 
 type Condition<T = any> = (arg: T) => boolean;
 
-class Validator<T> {
+abstract class Validator<T> {
   constructor(
     public validate: Condition<T>,
     public exception: Exception = DEFAULT_EXCEPTION
