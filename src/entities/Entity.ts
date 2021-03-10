5122e30ec69ddab2f6499419665d5dc6d7370d76
chlbri <chlbri.blac@gmail.com>
2021-03-10 08:09:14 +0000

    Remove __name from Entity

--- src/entities/Entity.ts
+++ src/entities/Entity.ts
@@ -1,12 +1,11 @@
 import { SimpleObject } from "../interfaces";
 
 export interface Entity {
-  __name: string;
   _id?: string;
 }
 
 export type SimpleEntity<T extends Entity> = {
-  [K in Exclude<keyof T, "__name" | "_id"> as SimpleObject<
+  [K in Exclude<keyof T, "_id"> as SimpleObject<
     T[K]
   > extends never
     ? never
