cf274d25592f413a116647bb0f90e201eebc52c6
chlbri <chlbri.blac@gmail.com>
2021-03-11 23:52:22 +0000

    add repo v1.1.2

---
+++ src/Interfaces/repo.ts
@@ -0,0 +1,24 @@
+import { Entity, WithId, WithoutId } from "../entities";
+import { PromiseReturnData } from "./ReturnData";
+
+export interface IRepo<T extends Entity> {
+  create: (value: T) => PromiseReturnData<T>;
+
+  createMany: (...values: T[]) => PromiseReturnData<T[]>;
+
+  readMany: (value: T, limit: number) => PromiseReturnData<T[]>;
+
+  readOne: (value: T) => PromiseReturnData<T>;
+
+  update: (
+    oldValue: T,
+    newValue: WithoutId<T>
+  ) => PromiseReturnData<T>;
+
+  updateMany: (
+    oldValue: T,
+    newValue: WithoutId<T>
+  ) => PromiseReturnData<T[]>;
+
+  delete: (value: T) => PromiseReturnData<T>;
+}
