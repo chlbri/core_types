8511d46740bbf3f60394b3ce57e085bcf14fed6e
chlbri <chlbri.blac@gmail.com>
2021-03-14 18:46:18 +0000

    tsc

--- dist/interfaces/repo.d.ts
+++ dist/interfaces/repo.d.ts
@@ -1,12 +1,12 @@
-import { Entity, WithoutId } from "../entities";
+import { Entity, WithId, WithoutId } from "../entities";
 import { PromiseReturnData } from "./ReturnData";
 export interface IRepo<T extends Entity> {
     create: (value: T) => PromiseReturnData<T>;
     createMany: (...values: T[]) => PromiseReturnData<T[]>;
-    upsert: (value: T) => PromiseReturnData<T>;
+    upsert: (value: WithId<Partial<T>>) => PromiseReturnData<T>;
     read: (value?: T, limit?: number) => PromiseReturnData<T[]>;
     readOne: (value: T) => PromiseReturnData<T>;
-    update: (oldValue: T, newValue: WithoutId<T>) => PromiseReturnData<T>;
-    updateMany: (oldValue: T, newValue: WithoutId<T>) => PromiseReturnData<T[]>;
+    update: (oldValue: T, newValue: WithoutId<Partial<T>>) => PromiseReturnData<T>;
+    updateMany: (oldValue: T, newValue: WithoutId<Partial<T>>) => PromiseReturnData<T[]>;
     delete: (value: T) => PromiseReturnData<T>;
 }
