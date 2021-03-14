556c6bfc00852eeb24b04c943b143e5c51cf4ec5
chlbri <chlbri.blac@gmail.com>
2021-03-14 19:53:24 +0000

    tsc

--- dist/interfaces/repo.d.ts
+++ dist/interfaces/repo.d.ts
@@ -10,8 +10,7 @@ export interface IRepo<T extends Entity> {
     createMany: (...values: T[]) => PromiseReturnData<WithId<T>[]>;
     upsert: (value: WithId<Partial<T>>) => PromiseReturnData<WithId<T>>;
     read: (search?: DataSearchOperations<T>, limit?: number) => PromiseReturnData<WithId<T>[]>;
-    readMany: (...ids: any[]) => PromiseReturnData<WithId<T>[]>;
-    readManyWhere: (ids: any[], search: WithoutId<T>) => PromiseReturnData<WithId<T>[]>;
+    readIds: (ids: any[], search: DataSearchOperations<WithoutId<T>>, limit?: number) => PromiseReturnData<WithId<T>[]>;
     readOne: (search: DataSearchOperations<T>) => PromiseReturnData<WithId<T>>;
     update: (search: DataSearchOperations<T>, newValue: WithoutId<Partial<T>>) => PromiseReturnData<WithId<T>[]>;
     updateMany: (...changes: UpdateMany<T>[]) => PromiseReturnData<WithId<T>[]>;
