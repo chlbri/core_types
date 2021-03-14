d291678b18aad04ea9ff3419ed2cccdeeb90155b
chlbri <chlbri.blac@gmail.com>
2021-03-14 18:53:57 +0000

    tsc

--- dist/interfaces/repo.d.ts
+++ dist/interfaces/repo.d.ts
@@ -1,12 +1,12 @@
 import { Entity, WithId, WithoutId } from "../entities";
 import { PromiseReturnData } from "./ReturnData";
 export interface IRepo<T extends Entity> {
-    create: (value: T) => PromiseReturnData<T>;
-    createMany: (...values: T[]) => PromiseReturnData<T[]>;
-    upsert: (value: WithId<Partial<T>>) => PromiseReturnData<T>;
-    read: (value?: T, limit?: number) => PromiseReturnData<T[]>;
-    readOne: (value: T) => PromiseReturnData<T>;
-    update: (oldValue: T, newValue: WithoutId<Partial<T>>) => PromiseReturnData<T>;
-    updateMany: (oldValue: T, newValue: WithoutId<Partial<T>>) => PromiseReturnData<T[]>;
-    delete: (value: T) => PromiseReturnData<T>;
+    create: (value: T) => PromiseReturnData<WithId<T>>;
+    createMany: (...values: T[]) => PromiseReturnData<WithId<T>[]>;
+    upsert: (value: WithId<Partial<T>>) => PromiseReturnData<WithId<T>>;
+    read: (value?: T, limit?: number) => PromiseReturnData<WithId<T>[]>;
+    readOne: (value: T) => PromiseReturnData<WithId<T>>;
+    update: (oldValue: T, newValue: WithoutId<Partial<T>>) => PromiseReturnData<WithId<T>>;
+    updateMany: (oldValue: T, newValue: WithoutId<Partial<T>>) => PromiseReturnData<WithId<T>[]>;
+    delete: (value: T) => PromiseReturnData<WithId<T>>;
 }
