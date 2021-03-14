cb3a5049245a78fc18eab2f8402ab3f24df7d0ce
chlbri <chlbri.blac@gmail.com>
2021-03-14 18:53:43 +0000

    Add Id to response

--- src/Interfaces/repo.ts
+++ src/Interfaces/repo.ts
@@ -2,25 +2,25 @@ import { Entity, WithId, WithoutId } from "../entities";
 import { PromiseReturnData } from "./ReturnData";
 
 export interface IRepo<T extends Entity> {
-  create: (value: T) => PromiseReturnData<T>;
+  create: (value: T) => PromiseReturnData<WithId<T>>;
 
-  createMany: (...values: T[]) => PromiseReturnData<T[]>;
+  createMany: (...values: T[]) => PromiseReturnData<WithId<T>[]>;
 
-  upsert: (value: WithId<Partial<T>>) => PromiseReturnData<T>;
+  upsert: (value: WithId<Partial<T>>) => PromiseReturnData<WithId<T>>;
 
-  read: (value?: T, limit?: number) => PromiseReturnData<T[]>;
+  read: (value?: T, limit?: number) => PromiseReturnData<WithId<T>[]>;
 
-  readOne: (value: T) => PromiseReturnData<T>;
+  readOne: (value: T) => PromiseReturnData<WithId<T>>;
 
   update: (
     oldValue: T,
     newValue: WithoutId<Partial<T>>
-  ) => PromiseReturnData<T>;
+  ) => PromiseReturnData<WithId<T>>;
 
   updateMany: (
     oldValue: T,
     newValue: WithoutId<Partial<T>>
-  ) => PromiseReturnData<T[]>;
+  ) => PromiseReturnData<WithId<T>[]>;
 
-  delete: (value: T) => PromiseReturnData<T>;
+  delete: (value: T) => PromiseReturnData<WithId<T>>;
 }
